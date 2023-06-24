c92dcc074c159b99ad4eb53a0500fd5debb541ac
LauTIPE <lautaro.efi@gmail.com>
2023-06-23 22:06:45 -0400

    Comentarios
    Y colores

--- app/src/main/java/com/unab/bootcamp/lautipe/profeboton/view/MainActivity.kt
+++ app/src/main/java/com/unab/bootcamp/lautipe/profeboton/view/MainActivity.kt
@@ -1,3 +1,4 @@
+// MainActivity.kt (Profesor)
 package com.unab.bootcamp.lautipe.profeboton.view
 
 import androidx.appcompat.app.AppCompatActivity
@@ -8,34 +9,41 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.FirebaseFirestore
 import com.unab.bootcamp.lautipe.profeboton.R
 
+// Definición de la clase MainActivity que hereda de AppCompatActivity
 class MainActivity : AppCompatActivity() {
 
+    // Constante para el tag de los logs
     companion object {
         private const val TAG = "MainActivity"
     }
 
-    private lateinit var db: FirebaseFirestore
+    // Declaración de las variables que se usarán en la clase
+    private lateinit var db: FirebaseFirestore // Instancia de Firestore
 
+    // Función que se ejecuta al crear la actividad
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        db = FirebaseFirestore.getInstance()
+        db = FirebaseFirestore.getInstance() // Inicialización de la instancia de Firestore
 
+        // Inicialización del botón y asignación de su comportamiento al hacer click
         val button: Button = findViewById(R.id.bottom_start_question)
         button.setOnClickListener {
+            // Creación de un HashMap con la pregunta
             val question = hashMapOf(
-                "timeStamp" to Timestamp.now(),
-                "responses" to arrayListOf<String>()
+                "timeStamp" to Timestamp.now(), // Marca de tiempo actual
+                "responses" to arrayListOf<String>() // Lista vacía para las respuestas
             )
 
+            // Añadir la pregunta a la colección de preguntas activas
             db.collection("Sessions").document("sessionID").collection("activeQuestions")
                 .add(question)
                 .addOnSuccessListener {
-                    Log.d(TAG, "DocumentSnapshot successfully written!")
+                    Log.d(TAG, "DocumentSnapshot successfully written!") // Log en caso de éxito
                 }
                 .addOnFailureListener { e ->
-                    Log.w(TAG, "Error writing document", e)
+                    Log.w(TAG, "Error writing document", e) // Log en caso de error
                 }
         }
     }
--- app/src/main/java/com/unab/bootcamp/lautipe/profeboton/view/StudentActivity.kt
+++ app/src/main/java/com/unab/bootcamp/lautipe/profeboton/view/StudentActivity.kt
@@ -12,77 +12,92 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.unab.bootcamp.lautipe.profeboton.R
 
+// Definición de la clase StudentActivity que hereda de AppCompatActivity
 class StudentActivity : AppCompatActivity() {
 
+    // Constante para el tag de los logs
     companion object {
         private const val TAG = "StudentActivity"
     }
 
+    // Declaración de las variables que se usarán en la clase
+    private lateinit var db: FirebaseFirestore  // Instancia de Firestore
+    private lateinit var lastQuestionId: String // ID de la última pregunta
 
-    private lateinit var db: FirebaseFirestore
-    private lateinit var lastQuestionId: String
-
+    // Función que se ejecuta al crear la actividad, en este caso crear un document sesión
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
 
-        db = FirebaseFirestore.getInstance()
+        db = FirebaseFirestore.getInstance() // Inicialización de la instancia de Firestore
 
+        // Creación de la referencia a la sesión actual
         val sessionRef = db.collection("Sessions").document("sessionID")
+
+        // Escucha de cambios en la colección de preguntas activas
         sessionRef.collection("activeQuestions")
-            .orderBy("timeStamp", Query.Direction.DESCENDING)
-            .limit(1)
+            .orderBy("timeStamp", Query.Direction.DESCENDING) // Ordenar por el timestamp en orden descendente
+            .limit(1) // Limitar los resultados a 1
             .addSnapshotListener { snapshots, e ->
-                if (e != null) {
+                if (e != null) {  // Si hay un error, se muestra en los logs y se detiene la ejecución
                     Log.w(TAG, "listen:error", e)
                     return@addSnapshotListener
                 }
 
+                // Si los snapshots no están vacíos
                 if (!snapshots!!.isEmpty) {
+                    // Iterar sobre los cambios en los documentos
                     for (dc in snapshots.documentChanges) {
+                        // Si se ha añadido un nuevo documento
                         if (dc.type == DocumentChange.Type.ADDED) {
-                            lastQuestionId = dc.document.id
-                            showPopup()
+                            lastQuestionId = dc.document.id // Guardar el ID de la última pregunta
+                            showPopup() // Mostrar el popup
                         }
                     }
                 }
             }
     }
 
+    // Función para mostrar el popup
     fun showPopup() {
-        val popup = PopupWindow(this)
-        val layout = layoutInflater.inflate(R.layout.popup_layout, null)
-        popup.contentView = layout
+        val popup = PopupWindow(this)  // Creación de una nueva ventana emergente
+        val layout = layoutInflater.inflate(R.layout.popup_layout, null)  // Inflado del layout del popup
+        popup.contentView = layout  // Asignación del layout al contenido del popup
 
+        // Inicialización de los botones del popup y asignación de su comportamiento al hacer click
         val yesButton: Button = layout.findViewById(R.id.button_option_one)
         yesButton.setOnClickListener {
-            sendResponse("up")
-            popup.dismiss()
+            sendResponse("up")  // Enviar la respuesta "up" al hacer click en "sí"
+            popup.dismiss()  // Cerrar el popup
         }
 
         val noButton: Button = layout.findViewById(R.id.button_option_two)
         noButton.setOnClickListener {
-            sendResponse("down")
-            popup.dismiss()
+            sendResponse("down") // Enviar la respuesta "down" al hacer click en "no"
+            popup.dismiss() // Cerrar el popup
         }
 
+        // Mostrar el popup en el centro de la pantalla
         popup.showAtLocation(findViewById(R.id.root_layout), Gravity.CENTER, 0, 0)
     }
+
+    // Función para enviar la respuesta
     fun sendResponse(choice: String) {
+        // Creación de un HashMap con la respuesta
         val response = hashMapOf(
             "studentChoice" to choice
         )
 
+        // Añadir la respuesta a la colección de respuestas de la pregunta activa
         db.collection("Sessions").document("sessionID")
             .collection("activeQuestions").document(lastQuestionId)
             .collection("responses")
             .add(response)
             .addOnSuccessListener {
-                Log.d(TAG, "Response successfully written!")
+                Log.d(TAG, "Response successfully written!")  // Log en caso de éxito
             }
             .addOnFailureListener { e ->
-                Log.w(TAG, "Error writing response", e)
+                Log.w(TAG, "Error writing response", e)  // Log en caso de error
             }
     }
 }
-
