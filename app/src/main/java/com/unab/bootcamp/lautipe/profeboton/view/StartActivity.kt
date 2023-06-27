bb3c23b40a1549a462590a977508a7869eda0259
LauTIPE <lautaro.efi@gmail.com>
2023-06-26 23:47:02 -0400

    Start_Activity
    App funcionando

--- app/src/main/java/com/unab/bootcamp/lautipe/profeboton/view/StartActivity.kt
+++ app/src/main/java/com/unab/bootcamp/lautipe/profeboton/view/StartActivity.kt
@@ -3,17 +3,18 @@ package com.unab.bootcamp.lautipe.profeboton.view
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.unab.bootcamp.lautipe.profeboton.R
 
-class LoginActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-        val teacherButton: Button = findViewById(R.id.button_teacher)
-        teacherButton.setOnClickListener {
+        val teacherText: TextView = findViewById(R.id.botton_teacher)
+        teacherText.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
